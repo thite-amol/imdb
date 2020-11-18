e53db3842ec2efc88ff493c9feb933b0e4878b34
Amol Thite <amol_thite@tekditechnologies.com>
2020-11-18 13:18:56 +0530

    Pagination support

--- movies/views.py
+++ movies/views.py
@@ -2,13 +2,33 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Movie, Genre
 from .serializers import MovieSerializer, GenreSerializer
+from rest_framework.pagination import LimitOffsetPagination
 
 # Create your views here.
+
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all().order_by('name')
     serializer_class = GenreSerializer
 
+
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all().order_by('name')
     serializer_class = MovieSerializer
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            qs = qs.filter(name__icontains=name)
+
+        genre = self.request.query_params.get('genre', None)
+        if genre is not None:
+            qs = qs.filter(genre__name__icontains=genre)
+
+        director = self.request.query_params.get('director', None)
+        if director is not None:
+            qs = qs.filter(director__icontains=director)
+
+        return qs
--- root/settings.py
+++ root/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
+}
